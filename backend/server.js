40621f027392769f23ce82e4f85bcadd63d118d5
Anurag Biswal <anurag.biswal0702@gmail.com>
2023-12-11 22:43:21 +0530

    handling errors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+
+app.use(notFound); // for handing errors found if above code throws an error
+app.use(errorHandler); // for handing miscellaneous errors found if above error is also not handled
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server started on PORT ${PORT}`));
