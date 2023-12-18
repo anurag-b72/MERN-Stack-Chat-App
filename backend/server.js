03ac72cf7a766d8c7ef758fb946c5415cb7eee29
Anurag Biswal <anurag.biswal0702@gmail.com>
2023-12-19 01:26:40 +0530

    backend: Send Message API ( One on One and Group Chat )

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const colors = require("colors");
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound); // for handing errors found if above code throws an error
 app.use(errorHandler); // for handing miscellaneous errors found if above error is also not handled
