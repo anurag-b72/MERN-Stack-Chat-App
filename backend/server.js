54dfa75b071a306cc06ce523388d7b3b3c7bbba0
Anurag Biswal <anurag.biswal0702@gmail.com>
2023-12-20 15:46:50 +0530

    backend: deployment code

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const colors = require("colors");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -14,13 +15,25 @@ const app = express();
 
 app.use(express.json()); // to access JSON data
 
-app.get("/", (req, res) => {
-  res.send("API is Running Successfully!");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------------Deployment--------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontent", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Successfully!");
+  });
+}
+// --------------------------Deployment--------------------
 
 app.use(notFound); // for handing errors found if above code throws an error
 app.use(errorHandler); // for handing miscellaneous errors found if above error is also not handled
